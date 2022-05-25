baa8e6e6d3e5b96a2410cd3c3b7c6e74fbd44690
Ольга <devyatovka.0993@mail.ru>
2022-05-25 21:07:21 +0300

    start_files

--- hello_django/hello_django/urls.py
+++ hello_django/hello_django/urls.py
@@ -14,10 +14,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path,include
 from.import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path(' ', include('hello_world.urls'))
+    path('',views.about),
+    path('home/',views.home),
+    path('reverse/', views.reverse,name='reversed'),
+    path('', include('hello_world.urls')),
 ]
