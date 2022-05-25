6110a5cf9465081de8c956cf8d9cb6f988e1bc63
Ольга <devyatovka.0993@mail.ru>
2022-05-25 12:19:41 +0300

    start_files

--- OlgaGeres_2021/OlgaGeres_2021/settings.py
+++ OlgaGeres_2021/OlgaGeres_2021/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'OlgaGeres_2021.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
