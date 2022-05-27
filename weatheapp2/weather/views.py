3b013078adcf2a9a9f55e2fff692d8e58e86fa0e
Ольга <devyatovka.0993@mail.ru>
2022-05-27 14:26:16 +0300

    connect_OpenWetherMap

--- weatheapp2/weather/views.py
+++ weatheapp2/weather/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
+import requests
 
 def index(request):
+    appid = '72926ea7ea0ca4e5887ebbe9834ba023'
+    url = 'https://api.openweathermap.org/data/2.5/weather?q={}&appid='+appid
+
+
+    city = 'London'
     return render(request, 'weather/index.html')
 
 
