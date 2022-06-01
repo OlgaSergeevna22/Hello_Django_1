b9f0b82d7dfe6897939de2123acb09b8dbc8631f
Ольга <devyatovka.0993@mail.ru>
2022-06-01 17:48:01 +0300

    add_Commet

--- hello_django/blog/models.py
+++ hello_django/blog/models.py
@@ -12,4 +12,11 @@ class Post(models.Model):
     last_modified = models.DateTimeField(auto_now=True)
     categories = models.ManyToManyField('Category', related_name='posts')
 
+class Comment(models.Model):
+    author = models.CharField(max_length=250)
+    body = models.TextField()
+    cteated_on = models.DateTimeField(auto_now_add=True)
+    post = models.ForeignKey('Post', on_delete=models.CASCADE)
+
+
 # Create your models here.
