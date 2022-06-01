60701fc2c97098c1a2bcc2f478e0278c74ef2e06
Ольга <devyatovka.0993@mail.ru>
2022-06-01 12:48:24 +0300

    add_project_index_project_detail

--- hello_django/project/views.py
+++ hello_django/project/views.py
@@ -3,6 +3,17 @@ from .models import Project
 
 def project_index (request):
     projects = Project.objects.all()
+    context = {
+        'projects':projects
+    }
+    return render(request, 'project_index.html', context)
+
+def project_detail(request,pk):
+    project = Project.objects.get(pk=pk)
+    context = {
+        'project': project
+    }
+    return render(request, 'project_detail.html', context)
 
 def func(request):
     return render(request,'index.html')
