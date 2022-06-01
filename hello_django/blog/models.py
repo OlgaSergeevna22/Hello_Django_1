6e63f0d846ad27c3558f3fb5ab2438f27b607f8d
Ольга <devyatovka.0993@mail.ru>
2022-06-01 17:34:05 +0300

    add_Post

--- hello_django/blog/models.py
+++ hello_django/blog/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
+
+class Category(models.Model):
+    name = models.CharField(max_length=30)
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=250)
+    body = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    last_modified = models.DateTimeField(auto_now=True)
+    categories = models.ManyToManyField('Category', related_name='posts')
+
 # Create your models here.
