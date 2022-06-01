f665b8dc5a8c4eb4923c8703bcd723f210da01ef
Ольга <devyatovka.0993@mail.ru>
2022-06-01 12:31:00 +0300

    add_QuerySet

--- hello_django/project/views.py
+++ hello_django/project/views.py
@@ -1,4 +1,8 @@
 from django.shortcuts import render
+from .models import Project
+
+def project_index (request):
+    projects = Project.objects.all()
 
 def func(request):
     return render(request,'index.html')
