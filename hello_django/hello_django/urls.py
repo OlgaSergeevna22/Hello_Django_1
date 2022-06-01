1cb78e8820316067c3ddd1ebf93ce0443094e222
Ольга <devyatovka.0993@mail.ru>
2022-06-01 20:47:01 +0300

    urls(2)_blog_ind_blog_det_base

--- hello_django/hello_django/urls.py
+++ hello_django/hello_django/urls.py
@@ -21,7 +21,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('about/',views.about),
     path('',include ('project.urls')),
-    path('',include ('blog.urls')),
+    path('blog/',include ('blog.urls')),
     path('home/',views.home),
     path('reverse/', views.reverse,name='reversed'),
 
