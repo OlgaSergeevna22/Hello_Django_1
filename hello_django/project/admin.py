6efa5078b790d60e32c33762c75184b8524d7ff5
Ольга <devyatovka.0993@mail.ru>
2022-06-01 21:03:20 +0300

    registrate_Category_Post

--- hello_django/project/admin.py
+++ hello_django/project/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
-from .models import Project
+from .models import Project, Post, Category
 
 admin.site.register(Project)
+admin.site.register(Post)
+admin.site.register(Category)
 
 # Register your models here.
