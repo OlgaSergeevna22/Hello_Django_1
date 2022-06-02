0496a1b1dfe3c0e410a1487165f7a404ebcf8d7d
Ольга <devyatovka.0993@mail.ru>
2022-06-02 19:41:34 +0300

    change

--- hello_django/blog/models.py
+++ hello_django/blog/models.py
@@ -15,7 +15,7 @@ class Post(models.Model):
 class Comment(models.Model):
     author = models.CharField(max_length=250)
     body = models.TextField()
-    cteated_on = models.DateTimeField(auto_now_add=True)
+    created_on = models.DateTimeField(auto_now_add=True)
     post = models.ForeignKey('Post', on_delete=models.CASCADE)
 
 
