601ce360ce1077f159a6d397e6b324a4e2844251
Ольга <devyatovka.0993@mail.ru>
2022-06-13 13:36:26 +0300

    change_home_1_main_page

--- OlgaGeres_2021/homework/urls.py
+++ OlgaGeres_2021/homework/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from .import views
 urlpatterns = [
-    path('homework/', views.homework),
-    path('', views.main_page)
+    path('home/', views.homework),
+    path('', views.main_page),
 ]
