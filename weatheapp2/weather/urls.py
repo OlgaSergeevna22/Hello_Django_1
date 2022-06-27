f46f00a462da556df5ad5a4ff3967b18f230c57e
Ольга <devyatovka.0993@mail.ru>
2022-06-27 14:08:00 +0300

    add_My_templ

--- weatheapp2/weather/urls.py
+++ weatheapp2/weather/urls.py
@@ -5,6 +5,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('', views.index),
+    path('home/', views.My_templ),
 
 ]
 
--- weatheapp2/weather/views.py
+++ weatheapp2/weather/views.py
@@ -17,3 +17,21 @@ def index(request):
     return render(request, 'weather/index.html', context)
 
 
+def My_templ(request):
+    appid = '72926ea7ea0ca4e5887ebbe9834ba023'
+    url = 'https://api.openweathermap.org/data/2.5/weather?q={}&units=metric&appid='+appid
+
+
+    city = 'Brest'
+    res = requests.get(url.format(city)).json()
+    city_info = {
+        'city':city,
+        'temp':res['main']['temp'],
+        'icon':res['weather'][0]['icon'],
+        'speed':res['wind']['speed'],
+        'cloud':res['clouds']['all']
+    }
+    context = {'info':city_info}
+    return render(request, 'weather/My_templ.html', context)
+
+
